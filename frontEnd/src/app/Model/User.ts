33b24de5f538b0d25dd4dc137fb69e35090714e4
PameCS <70495697+PameCS@users.noreply.github.com>
2020-10-05 23:46:44 -0600

    Modulo aulas

--- frontEnd/src/app/Model/User.ts
+++
@@ -1,9 +0,0 @@
-export class User{
-    constructor(){}
-    // tslint:disable-next-line: variable-name
-    pk_idUser: number;
-    password: string;
-    name: string;
-    lastName: string;
-    type: string;
-}
